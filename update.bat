fb24f2090e5861fd619c75240e2ef585511a597c
CDK6182CHR <794288384@qq.com>
2019-02-04 23:03:35 +0800

    'update'

---
+++ update.bat
@@ -0,0 +1,13 @@
+copy ..\Timetable_new\direction.py direction.py
+copy ..\Timetable_new\utility.py utility.py
+copy ..\Timetable_new\checi3.py checi3.py
+copy ..\Timetable_new\connect2.py connect2.py
+
+git add direction.py
+git add utility.py
+git add checi3.py
+git add connect2.py
+git add update.bat
+
+git commit -m 'update'
+git push origin master
